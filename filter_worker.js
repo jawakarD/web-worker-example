4727791ee68f85dff3a6d49abc08cb361ec88c20
jawakarD <jawakarsmith@gmail.com>
2020-02-18 11:21:04 +0530

    Init commit 🥳

---
+++ filter_worker.js
@@ -0,0 +1,35 @@
+const Filters = {};
+
+Filters.none = function none() {};
+
+Filters.grayscale = ({data: d}) => {
+  for (let i = 0; i < d.length; i += 4) {
+    const [r, g, b] = [d[i], d[i + 1], d[i + 2]];
+
+    // CIE luminance for the RGB
+    // The human eye is bad at seeing red and blue, so we de-emphasize them.
+    d[i] = d[i + 1] = d[i + 2] = 0.2126 * r + 0.7152 * g + 0.0722 * b;
+  }
+};
+
+Filters.brighten = ({data: d}) => {
+  for (let i = 0; i < d.length; ++i) {
+    d[i] *= 1.2;
+  }
+};
+
+Filters.threshold = ({data: d}) => {
+  for (var i = 0; i < d.length; i += 4) {
+    var r = d[i];
+    var g = d[i + 1];
+    var b = d[i + 2];
+    var v = 0.2126 * r + 0.7152 * g + 0.0722 * b >= 10 ? 255 : 0;
+    d[i] = d[i + 1] = d[i + 2] = v;
+  }
+};
+
+onmessage = e => {
+  const {imageData, filter} = e.data;
+  Filters[filter](imageData);
+  postMessage(imageData, [imageData.data.buffer]);
+};
---
+++ script.js
@@ -0,0 +1,45 @@
+//document.addEventListener('DOMContentLoaded', () => {
+const worker = new Worker('filter_worker.js');
+
+const inputImage = document.getElementById('image-to-filter');
+const outputC = document.getElementById('output');
+const filter = document.querySelector('#filter');
+const oCtx = outputC.getContext('2d');
+const img = new Image();
+let imageData;
+
+const sendDataToWorker = () =>
+  worker.postMessage({imageData, filter: filter.value});
+
+const receiveFromWorker = e => oCtx.putImageData(e.data, 0, 0);
+
+worker.onmessage = receiveFromWorker;
+
+const drawImg = () => {
+  outputC.height = img.height;
+  outputC.width = img.width;
+
+  console.log(img.height);
+  oCtx.drawImage(img, 0, 0);
+  imageData = oCtx.getImageData(0, 0, img.height, img.width);
+  sendDataToWorker();
+};
+
+inputImage.addEventListener('change', e => {
+  const file = e.target.files[0];
+
+  const reader = new FileReader();
+  reader.onload = e => {
+    img.src = e.target.result;
+    img.style.display = 'none';
+    img.onload = () => {
+      document.body.appendChild(img);
+      drawImg();
+    };
+  };
+
+  reader.readAsDataURL(file);
+});
+
+filter.addEventListener('change', e => sendDataToWorker());
+//});
